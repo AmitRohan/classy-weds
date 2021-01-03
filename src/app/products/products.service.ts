7c63dc14b4d2dcacb119caf7e11227dc55e69a8d
Amit Rohan <a17rohan@egmail.com>
2021-01-04 05:14:36 +0530

    Added Product List & Product Details API

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -12,47 +12,29 @@ export class ProductsService {
     
     }
 
-    private baseUrl = environment.baseUrl + '/products';
+    private baseUrl = environment.baseUrl + '/api/v1';
 
-    fetchProductList(callback) {
+    fetchProductList(serviceName,callback) {
+        var _baseUrl = this.baseUrl + '/services/'+ serviceName +'/products';
         this.apiservice.apicall({
-                url: this.baseUrl
+                url: _baseUrl
+                , httpMethod: 'post'
+            }, (result) => {
+                    console.log('GOT fetchProductList', result);
+                    callback(result);
+          }, (error) => this.defaultErrorHandler(error, callback));
+    }
+
+    fetchProductDetails(productId,callback) {
+        var _baseUrl = this.baseUrl + '/products/'+productId;
+        this.apiservice.apicall({
+                url: _baseUrl
                 , httpMethod: 'get'
             }, (result) => {
-            console.log('GOT fetchProductList', result);
-            callback(result);
+                    console.log('GOT fetchProductDetails', result);
+                    callback(result);
           }, (error) => this.defaultErrorHandler(error, callback));
     }
-
-    fetchTripListByUserId(userId: string, callback) {
-        if (environment.offlineFirst) {
-            setTimeout(callback([]), 1000);
-            return;
-        }
-        const body = {
-            'sortBy': 'desc',
-            'sortOn': 'startTime',
-            'filter': [{
-                'key': 'userId',
-                'requestFilterType': 'EQUAL',
-                'value': userId
-            }]
-        };
-        const token = sessionStorage.getItem('outh_key');
-        const auth = 'Bearer ' + token;
-        // const url_otp = baseUrl + "login/generateOTP"
-        this.apiservice.apicall({
-                url: this.baseUrl + '/v1/trips?pageSize=100&pageNum=0'
-                , params : body
-                , httpMethod: 'postAuth'
-                , Authorization: auth
-            }, (result) => {
-            console.log('GOT getTripsDataForUser', result);
-            callback(result);
-          }, (error) => this.defaultErrorHandler(error, callback));
-    }
-
-
 
     defaultErrorHandler(error, callback) {
         console.log(error, ' == error');
