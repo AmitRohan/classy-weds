f04ceb881f0c5701ce3f36c4323bfeaeb6d83b98
Amit Rohan <a17rohan@egmail.com>
2021-01-18 16:45:57 +0530

    Added API Calls

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -37,6 +37,69 @@ export class ProductsService {
           }, (error) => this.defaultErrorHandler(error, callback));
     }
 
+    fetchReviewListForProduct(productId,callback) {
+        var _baseUrl = this.baseUrl + '/products/'+productId +'/reviews';
+        this.apiservice.apicall({
+                url: _baseUrl
+                , httpMethod: 'get'
+            }, (result) => {
+                    console.log('GOT fetchReviewListForProduct', result);
+                    callback(result);
+          }, (error) => this.defaultErrorHandler(error, callback));
+    }
+
+    addReviewForProduct(productId,reviewToAdd,callback) {
+        reviewToAdd = {"rating":3, "tags":["Value For Money", "Amazing Food", "Hospitality"], "user_id":"31", "message":"this is awesome"};
+        var _baseUrl = this.baseUrl + '/products/'+productId +'/newReview';
+        this.apiservice.apicall({
+                url: _baseUrl
+                , httpMethod: 'post'
+                , params : reviewToAdd
+            }, (result) => {
+                    console.log('GOT addReviewForProduct', result);
+                    callback(result);
+          }, (error) => this.defaultErrorHandler(error, callback));
+    }
+
+    requestCallBack(phoneNumber,mode,productId = null,name = null,callback) {
+        var _baseUrl = environment.baseUrl + '/phonenumber';
+
+        var body = {
+            phone : phoneNumber,
+            mode : mode,
+            product_id : productId,
+            name : name
+        }
+        this.apiservice.apicall({
+                url: _baseUrl
+                , httpMethod: 'post'
+                , params : body
+            }, (result) => {
+                    console.log('GOT requestCallBack', result);
+                    callback(result);
+          }, (error) => this.defaultErrorHandler(error, callback));
+    }
+
+    contactUs(name,phone,email,message,pwc,callback) {
+        var _baseUrl = environment.baseUrl + '/contactus';
+
+        var body = {
+            message,
+            name,
+            email,
+            phone,
+            pwc
+        }
+        this.apiservice.apicall({
+                url: _baseUrl
+                , httpMethod: 'post'
+                , params : body
+            }, (result) => {
+                    console.log('GOT contactUs', result);
+                    callback(result);
+          }, (error) => this.defaultErrorHandler(error, callback));
+    }
+
     defaultErrorHandler(error, callback) {
         console.log(error, ' == error');
         // alert(error.error.message);
