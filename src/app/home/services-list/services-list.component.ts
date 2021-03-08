a72470478726cabb1d92084c97b6c202dc425b87
Amit Rohan <a17rohan@gmail.com>
2021-03-09 03:48:09 +0530

    Added API call to update state

--- src/app/home/services-list/services-list.component.ts
+++ src/app/home/services-list/services-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, Input, AfterViewInit, AfterContentChecked} from '@angular/core';
 import { HomeService } from '../home.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {ClassyService} from "../home.types";
 
 declare const $: any;
 declare const Morris: any;
@@ -14,28 +15,29 @@ declare var H: any;
   encapsulation: ViewEncapsulation.None
 })
 export class ServicesListComponent implements OnInit , AfterViewInit , AfterContentChecked {
-  serviceList: Array<any> = [
-    {
-      icon : `service_photography.png`
-      , name : `PHOTOGRAPHY`
-    },{
-      icon : `service_music_dj.png`
-      , name : `MUSIC AND DJ`
-    },{
-      icon : `service_mehndi.png`
-      , name : `MEHNDI`
-    },{
-      icon : `service_makeup.png`
-      , name : `MAKEUP`
-    },{
-      icon : `service_venue.png`
-      , name : `VENUE`
-    },{
-      icon : `service_wedding.png`
-      , name : `WEDDING CARD`
-    }
+  serviceList: Array<ClassyService> = [
+
   ];
 
+//   [ {
+//     icon : `service_photography.png`
+//     , name : `PHOTOGRAPHY`
+//   },{
+//   icon : `service_music_dj.png`
+// , name : `MUSIC AND DJ`
+// },{
+//   icon : `service_mehndi.png`
+//       , name : `MEHNDI`
+// },{
+//   icon : `service_makeup.png`
+//       , name : `MAKEUP`
+// },{
+//   icon : `service_venue.png`
+//       , name : `VENUE`
+// },{
+//   icon : `service_wedding.png`
+//       , name : `WEDDING CARD`
+// }]
 
   constructor(
       private dashboardData: HomeService,
@@ -44,7 +46,7 @@ export class ServicesListComponent implements OnInit , AfterViewInit , AfterCont
   ) { }
 
     ngOnInit() {
-
+        this.loadServices();
       }
 
       ngAfterViewInit(): void {
@@ -55,7 +57,21 @@ export class ServicesListComponent implements OnInit , AfterViewInit , AfterCont
 
       }
 
-      openService(service : any){
-        this.router.navigateByUrl('/services/'+service.name.split(' ').join('_'));
+      openService(service : ClassyService){
+        this.router.navigateByUrl('/services/'+service.id);
       }
+
+      private loadServices() {
+        this.dashboardData.getServices((resp,error)=>{
+          console.log(resp,error)
+          if(error){
+            return;
+          }
+          this.serviceList = (resp || []).filter((_ , i) => i < 6);
+        })
+      }
+
+  toLocaleNameFormat(name: string = '') : string {
+    return name.toUpperCase();
+  }
 }
