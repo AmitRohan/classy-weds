03078ae6e30f4e64844e552c346fa9f907aa0a7e
varun-classyweds <65715823+varun-classyweds@users.noreply.github.com>
2021-03-14 02:02:47 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   merchantName : 'Afaq Prod',
   production : true,
   // baseUrl : 'http://VPSINGH.COM',
-  baseUrl : 'https://classywed.com'
+  baseUrl : 'https://www.classywed.com'
 };
 
 // http://13.232.158.180:5555
