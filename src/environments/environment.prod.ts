ba4a2bac01d8be1cab9b8fc5dbe9cd21e9f730de
Amit Rohan <a17rohan@gmail.com>
2021-03-22 18:19:52 +0530

    Tmp fox for CORS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   merchantName : 'Afaq Prod',
   production : true,
   // baseUrl : 'http://VPSINGH.COM',
-  baseUrl : 'http://classywed.com'
+  // baseUrl : 'http://classywed.com',
+  baseUrl : 'http://13.232.158.180:5555'
 };
-
-// http://13.232.158.180:5555
