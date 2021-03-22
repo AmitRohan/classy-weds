79bb861483dc4dd3f6a2527d43880b090ad38e08
varun-classyweds <65715823+varun-classyweds@users.noreply.github.com>
2021-03-22 18:46:48 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production : true,
   // baseUrl : 'http://VPSINGH.COM',
 //   baseUrl : 'http://classywed.com'
-  baseUrl : 'http://13.232.158.180:5555'
+  baseUrl : 'https://13.232.158.180:5555'
 };
 
 // http://13.232.158.180:5555
