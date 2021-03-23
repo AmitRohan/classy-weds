955a6e0c0f8a8bec711afa31758cc2bb67a809e6
Amit Rohan <a17rohan@gmail.com>
2021-03-24 00:41:38 +0530

    Endpoint updation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   merchantName : 'Afaq Prod',
   production : true,
   // baseUrl : 'http://VPSINGH.COM',
-  // baseUrl : 'https://cms.classywed.com',
-  baseUrl : 'http://13.232.158.180:5555'
+  baseUrl : 'https://cms.classywed.com'
+  // baseUrl : 'http://13.232.158.180:5555'
 };
