52963df5cd1fc930e61202a2d5119eb6d93cb96d
Amit Rohan <a17rohan@gmail.com>
2021-03-24 00:38:51 +0530

    Cleanup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   merchantName : 'Afaq Prod',
   production : true,
   // baseUrl : 'http://VPSINGH.COM',
-  // baseUrl : 'http://classywed.com',
+  // baseUrl : 'https://cms.classywed.com',
   baseUrl : 'http://13.232.158.180:5555'
 };
